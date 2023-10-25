1c4ca1e549f72b12fcc08790e7465e25b0b87cd5
gustavocastrow <gustavocastrocs@gmail.com>
2023-10-25 16:21:18 -0300

    feat:01-11 Banco de dados H2, camadas

---
+++ bootcamp-spring3.0/backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscatalog.resources;
+
+import java.util.List;
+
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscatalog.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories") //dentro de categories teremos varios endpoints (CRUD)
+public class CategoryResource {
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<List <Category>> findAll(){
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
+
+//service -> repository -> banco de dados -> trouxe os objs -> instanciou -> trouxe p ca e guardou na List
---
+++ bootcamp-spring3.0/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service //Registrar a classe CategoryService como um componente que vai participar do sistema
+//de injeção de dependencia do spring
+public class CategoryService {
+    @Autowired
+    private CategoryRepository repository;
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+}
+
+
