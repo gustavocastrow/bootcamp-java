be8591ecf19b16acabda849823e75b67678bc095
Gustavo Castro <gustavo.silva@revizia.com.br>
2024-06-15 09:12:52 -0300

    feat:class69

--- oop/src/main/java/application/Program67.java
+++ oop/src/main/java/application/Program67.java
@@ -22,7 +22,25 @@ public class Program67 {
         product.quantity = sc.nextInt();
 
 
-        System.out.println(product);
+        System.out.println("Product data: " + product);
+        System.out.println();
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated product data: " + product);
+
 
         sc.close();
     }
